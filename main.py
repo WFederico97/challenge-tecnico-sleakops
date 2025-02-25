f2fdcb3d53c2fd07bc14bb4a304a842b87f5db30
Federico Wuthrich <wuthrich09@gmail.com>
2025-02-24 22:51:56 -0300

    Refactored main app

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from typing import Union
+from fastapi_pagination import add_pagination
+from Routes.ProductRoutes import router as product_router
+from Routes.LoadAwsDataRoute import router as load_aws_data_router
+from Db.db import create_tables
 from fastapi import FastAPI
 
 app = FastAPI()
+create_tables()
 
-@app.get("/")
-async def home():
-    return {"hello" : "World"}
+app.include_router(product_router)
+app.include_router(load_aws_data_router)
+
+add_pagination(app)
